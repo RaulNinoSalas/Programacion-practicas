b62483e97489c61bfc20f33d98ee7c0dd5746533
RaulNinoSalas <32308547+RaulNinoSalas@users.noreply.github.com>
2017-11-15 19:51:03 +0100

    Delete P7E3.py

--- P7E3.py
+++
@@ -1,10 +0,0 @@
-"""RAUL NIÑO SALAS DAW 1
-Ejercicio_03
-Escribe un programa que lea una frase, y la pase como parámetro a un procedimiento, y éste
-debe mostrar la frase con un carácter en cada línea. """
-
-def a ():
-    texto=raw_input ("Escribe una frase: ")
-    for i in range (len(texto)):
-        print texto[i]
-a( )
