67389f3934f67f71ed58b08e1d42a1c1a88b4564
RaulNinoSalas <32308547+RaulNinoSalas@users.noreply.github.com>
2017-11-15 19:43:34 +0100

    Add files via upload

---
+++ P7E1.py
@@ -0,0 +1,12 @@
+"""RAUL NIÑO SALAS DAW 1
+Ejercicio_01
+Escribe un programa que pida un texto por pantalla, este texto lo pase como parámetro a un
+procedimiento, y éste lo imprima primero todo en minúsculas y luego todo en mayúsculas. """
+
+
+
+def a():
+    texto=raw_input ("Escribe un texto: ")
+    print texto.upper()
+    print texto.lower()
+a()
