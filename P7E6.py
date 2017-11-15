bbcf3dfc8bba377d06536523fbe03b4c6ac6cfc6
RaulNinoSalas <32308547+RaulNinoSalas@users.noreply.github.com>
2017-11-15 19:54:53 +0100

    Delete P7E6.py

--- P7E6.py
+++
@@ -1,14 +0,0 @@
-"""RAUL NIÑO SALAS DAW 1
-Ejercicio_06
-Escribe un programa que lea el nombre de una persona y un carácter, le pase estos datos a una
-función que comprobará si dicho carácter está en su nombre. El resultado de dicha función lo
-imprimirá el programa principal por pantalla. """
-
-nombre=raw_input("Dime tu nombre ")
-letra=raw_input("Dame una letra ")
-
-
-def cuantos ():
-    conta1=nombre.count(letra)
-    print conta1
-cuantos()
